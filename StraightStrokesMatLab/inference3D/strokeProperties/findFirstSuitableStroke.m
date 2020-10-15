af9f041d2407d07474375d737c4512bd3db2dcbf
ygryadit <61828485+ygryadit@users.noreply.github.com>
2020-10-15 12:45:28 +0100

    Update findFirstSuitableStroke.m
    Fix of an automatic skipping of the very first stroke.

--- StraightStrokesMatLab/inference3D/strokeProperties/findFirstSuitableStroke.m
+++ StraightStrokesMatLab/inference3D/strokeProperties/findFirstSuitableStroke.m
@@ -5,8 +5,18 @@
 function si = findFirstSuitableStroke(strokes_topology, intersections)
 
     si = 1;
-    three_dirs = false;
-    num_enough_likely = false;
+    three_dirs = true;
+    num_enough_likely = true;
+    
+    if ismember(strokes_topology(si).line_group, [1,2,3])
+        likely_inter_mask = cat(1,intersections(strokes_topology(si).indcs_intrsctns).likely);
+        intersecting_strokes = strokes_topology(si).indcs_intrsctng_strks(likely_inter_mask);
+        num_enough_likely = length(intersecting_strokes) > 1;
+        groups = cat(1,strokes_topology(intersecting_strokes).line_group);
+        others = setdiff([1,2,3], strokes_topology(si).line_group);
+        three_dirs = ismember(others(1), groups) | ismember(others(2), groups);
+    end
+    
     while isempty(strokes_topology(si).line_group) || ...
           ~ismember(strokes_topology(si).line_group, [1,2,3])  || ...
           (strokes_topology(si).mean_pressure < 0.05) || ...
